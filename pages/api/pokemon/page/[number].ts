febbf6cc88aa483aed662da4d58d823189f6331a
Johan Steenbekkers <steenbekkers@weprovide.com>
2021-06-18 13:36:50 +0200

    Load more functionality

---
+++ pages/api/pokemon/page/[number].ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PokemonListItemInterface } from "../../../../types/pokemon";
+
+interface DataInterface {
+    pokemon: PokemonListItemInterface[];
+}
+
+const handler = async (request: NextApiRequest, response: NextApiResponse<DataInterface>) => {
+    const pageNumber = parseInt(request.query.number instanceof Array ? request.query.number.join("") : request.query.number);
+    const pageSize = parseInt(process.env.PAGE_SIZE ?? "20");
+    const offset = pageSize * (pageNumber - 1);
+
+    const result = await fetch(`${process.env.API_HOST}/pokemon?limit=${pageSize}&offset=${offset}`).then(response => response.json());
+
+    response.status(200).json(result.results);
+}
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,46 @@
 import { GetServerSideProps, NextPage } from 'next'
+import { useState } from 'react'
 import PokemonList from '../components/pokemon/PokemonList'
 import { PokemonListItemInterface } from '../types/pokemon'
 
 interface HomeProps {
+  initialPage: number;
   pokemon: PokemonListItemInterface[];
 }
 
-const Home: NextPage<HomeProps> = ({ pokemon }) => {
+const Home: NextPage<HomeProps> = ({ pokemon, initialPage }) => {
+  const [pokemonList, setPokemonList] = useState<PokemonListItemInterface[]>(pokemon);
+  const [currentPage, setCurrentPage] = useState<number>(initialPage);
+
+  const loadMore = () => {
+    const nextPage = currentPage + 1;
+    const pokemon = fetch(
+      `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/pokemon/page/${nextPage}`
+    ).then(response => response.json()).then(data => {
+
+      setPokemonList([
+        ...pokemonList,
+        ...data
+      ]);
+      setCurrentPage(nextPage);
+    });
+  }
+
   return (
-      <PokemonList pokemon={pokemon} />
+      <>
+        <PokemonList pokemon={pokemonList} />
+        <button type="button" onClick={loadMore}>Load more</button>
+      </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const pokemon = await fetch(`${process.env.API_HOST}/pokemon?limit=20&offset=0`).then(response => response.json());
+  const currentPage = 1;
+  const pokemon = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/pokemon/page/${currentPage}`).then(response => response.json());
   return {
     props: {
-      pokemon: pokemon?.results
+      initialPage: currentPage,
+      pokemon: pokemon
     }
   }
 }
