a05ca1d24f6a34e285b197dd0a2ab90508ab0db9
Mike Donkers <mike@weprovide.com>
2021-07-02 16:16:18 +0200

    add next eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
         es6: true,
       },
       extends: [
+        'next',
         'eslint:recommended',
         'plugin:prettier/recommended',
         'plugin:@typescript-eslint/recommended', // TypeScript rules
@@ -38,6 +39,8 @@ module.exports = {
 
         // Why would you want unused vars?
         '@typescript-eslint/no-unused-vars': ['error'],
+
+        '@next/next/no-img-element': 'off',
 
         // I suggest this setting for requiring return types on functions only where useful
         '@typescript-eslint/explicit-function-return-type': [
